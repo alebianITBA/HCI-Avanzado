51064b5862813622094e5aa3c0c0791f1baf9595
alebian <alebezdjian@Alejandros-MacBook-Pro.local>
2015-06-24 18:56:10 -0300

    Added library and assets

---
+++ TPE-Lumberjack/Lumberjack/Assets/Scripts/Countdown.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour {
+
+	public float minutesLeft = 5;
+	private float secondsLeft;
+	public GameObject gameOverPanel;
+	// Use this for initialization
+	void Start () {
+		secondsLeft = minutesLeft * 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		secondsLeft -= Time.deltaTime;
+		if (secondsLeft >= 0) {
+			minutesLeft = secondsLeft / 60;
+			int minutes = (int)minutesLeft;
+			int seconds = ((int)secondsLeft) % 60;
+			gameObject.GetComponentInChildren<Text> ().text = minutes.ToString () + ":" + ((seconds < 10) ? "0" : "") + seconds.ToString ();
+		} else {
+			gameOverPanel.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ TPE-Lumberjack/Lumberjack/Assets/Scripts/HideIntro.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class HideIntro : MonoBehaviour {
+
+	public GameObject countdownPanel;
+
+	private Controller controller;
+	// Use this for initialization
+	void Start () {
+		controller = new Controller ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame ();
+		if (frame.Hands != null && !frame.Hands.IsEmpty) {
+			this.gameObject.SetActive(false);
+			countdownPanel.SetActive(true);
+		}
+	}
+}
